49c36a537c364bed28a0a92c20c5478062ec941b
emirontop1 <dincemr@gmail.com>
2026-06-17 12:52:53 +0300

    Delete src directory

--- src/Theme.lua
+++
@@ -1,9 +0,0 @@
--- Global veya lokal bir tabloda temayı tutuyoruz (Birleştiğinde Init.lua buna erişebilecek)
-_G.LibraryTheme = {
-    MainBg = Color3.fromRGB(20, 20, 20),
-    AccentColor = Color3.fromRGB(43, 108, 176),
-    TextColor = Color3.fromRGB(255, 255, 255),
-    ElementBg = Color3.fromRGB(30, 30, 30),
-    Font = Enum.Font.GothamSemibold
-}
-
